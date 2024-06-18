e3b9ba55204c7ab5d43e58ca1800bee14cd685e9
loyal812 <jh.chan0812@gmail.com>
2024-06-18 11:31:55 -0400

    feat: implement the retrieve job api

--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -7,13 +7,16 @@ import HeaderBrand from "@/components/Header/Brand";
 import Input from "@/components/InputForms";
 import Button from "@/components/Buttons";
 import Table from "@/components/Table";
+import { showSweetAlert } from "../../../utils/showSweetAlert";
+import axios from "axios";
 
 interface DashboardContainerProps { }
 
 const DashboardContainer: FC<DashboardContainerProps> = ({ }) => {
     const [search, setSearch] = useState("");
+    const [data, setData] = useState<any[]>([])
 
-    const data = [
+    const data1 = [
         { job_name: 'Licensed Professional Counselor', company_name: "LifeStance Health", min_salary: '$90,000', max_salary: "$114,000", salary_type: "Annual" },
         { job_name: 'Property Manager', company_name: "WinnCompanies", min_salary: '$18', max_salary: "$23", salary_type: "Hourly" },
     ];
@@ -26,8 +29,57 @@ const DashboardContainer: FC<DashboardContainerProps> = ({ }) => {
         { header: 'Compensation Type', key: 'salary_type' },
     ];
 
-    const retrieveJob = () => {
+    function validate() {
+        if (!search) {
+            showSweetAlert({
+                title: 'Validataion Error',
+                text: "Please fill job search field!",
+                icon: 'error',
+            });
+            return false;
+        }
 
+        return true;
+    }
+
+    const retrieveJob = async () => {
+        const endpoint_url = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/job/retrieve_job`
+
+        var validate_result = validate();
+
+        if (validate_result) {
+            try {
+                const response = await axios.post(endpoint_url, {
+                    "job_name": search
+                })
+
+                if (response.data && response.data.length > 0) {
+                    let tmp_jobs: any[] = []
+
+                    response.data.map((job: any) => {
+                        tmp_jobs.push({
+                            job_name: job.job_name,
+                            company_name: job.company_name,
+                            min_salary: job.minimum_compensation,
+                            max_salary: job.maximum_compensation,
+                            salary_type: job.compensation_type
+                        })
+                    })
+
+                    setData(tmp_jobs)
+                } else {
+                    setData([])
+                }
+
+
+            } catch (error) {
+                showSweetAlert({
+                    title: 'Server Connect Error!',
+                    text: 'Server Connection Error',
+                    icon: 'error',
+                });
+            }
+        }
     }
 
     return (
